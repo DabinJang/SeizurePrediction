7079191e18a13e116c1d7a842d528012c1a56e41
Jang Dabin <59811654+DabinJang@users.noreply.github.com>
2023-11-06 04:05:57 +0900

    extract dataloader from train file.

---
+++ dataloader.py
@@ -0,0 +1,46 @@
+import numpy as np
+from tensorflow.keras.utils import Sequence
+from read_dataset import GetBatchIndexes, LoadDataset, Interval2Segments_v2, Segments2Data_v2
+
+class DataLoader(Sequence):
+    def __init__(self, type_1_data, type_2_data, type_3_data, batch_size, shuffle=True):
+        self.type_1_data = type_1_data
+        self.type_2_data = type_2_data
+        self.type_3_data = type_3_data
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        
+        self.x = self.sampled_data(samle_rate=[1,1,.4])
+        self.indices = np.arange(len(self.x))
+        
+        if self.shuffle == True:
+            np.random.shuffle(self.indices)
+        
+    def sampled_data(self, sample_rate):
+        type_1_sampled_len = int(len(self.type_1_data)*sample_rate[0])
+        type_2_sampled_len = int(len(self.type_2_data)*sample_rate[1])
+        type_3_sampled_len = int(len(self.type_3_data)*sample_rate[2])
+
+        type_1_sampled = self.type_1_data[np.random.choice(len(self.type_1_data), type_1_sampled_len, replace=False)]
+        type_2_sampled = self.type_2_data[np.random.choice(len(self.type_2_data), type_2_sampled_len, replace=False)]
+        type_3_sampled = self.type_3_data[np.random.choice(len(self.type_3_data), type_3_sampled_len, replace=False)]
+
+        return type_1_sampled + type_2_sampled + type_3_sampled
+    
+    def __len__(self):
+	    return np.ceil(len(self.x) / self.batch_size)
+    
+    def __getitem__(self, idx):
+        indices = self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
+        input_seg = [self.x[i] for i in indices]
+        batch_x, batch_y = Segments2Data_v2(input_seg)
+
+        return batch_x, batch_y
+    
+    def on_epoch_end(self):
+        if self.shuffle == True:
+            np.random.shuffle(self.indices)
+            
+
+if __name__=="__main__":
+    pass
